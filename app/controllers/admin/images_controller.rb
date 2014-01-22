db21bca5f2385c5b42df920382210beab0251b81
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-22 16:00:25 +0100

    Select variant for image upload

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -29,6 +29,7 @@ class Admin::ImagesController < AdminController
       @master_variant = Variant.find(@variant.master_id)
       @cancel_path = admin_product_variant_path(I18n.locale, @master_variant, @variant)
     end
+    @variants = Variant.where(master_id: @master_variant.id)
     session[:variant_images_page] = request.env['HTTP_REFERER'] ||
                                       admin_product_images_path(I18n.locale, @master_variant)
 
