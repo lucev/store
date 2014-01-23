bd47f6de71fe04caff80f699be606164eba552df
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-23 18:27:19 +0100

    Rename variable

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -2,7 +2,7 @@ class VariantsController < ApplicationController
 
   def show
     @variant = Variant.find(params[:id])
-    @product_variants = Variant.where(master_id: @variant.master_id)
+    @variants = Variant.where(master_id: @variant.master_id)
   end
   
 end
