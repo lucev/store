1fc967560f36dd63909c1fa5182d455f798fb46e
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-23 21:48:45 +0100

    Always show current session cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,8 +13,6 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-    @cart = Cart.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @cart }
