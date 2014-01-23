9af85d7220c5c1c5d8582394d5351a0e7526b5e2
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-23 16:15:37 +0100

    Change variants controller

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -1,8 +1,8 @@
 class VariantsController < ApplicationController
 
   def show
-    @master_variant = Variant.find(params[:id])
-    @variants = Variant.where(master_id: @master_variant.id)
+    @variant = Variant.find(params[:id])
+    @product_variants = Variant.where(master_id: @variant.master_id)
   end
   
 end
