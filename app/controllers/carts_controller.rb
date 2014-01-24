f19a48fcb5a3fbf590aa3b843ff6c9272d8428e7
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-24 15:01:30 +0100

    Change destroy cart method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -70,11 +70,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart = Cart.find(params[:id])
-    @cart.destroy
+    current_cart.destroy
 
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to show_cart_path }
       format.json { head :no_content }
     end
   end
