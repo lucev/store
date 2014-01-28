25b8218f73c767e7ea775738d559410a35b02f3e
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-28 18:13:35 +0100

    Add product taxonomies controller and views

---
+++ app/controllers/admin/product_taxonomies_controller.rb
@@ -0,0 +1,44 @@
+class Admin::ProductTaxonomiesController < AdminController
+  include Admin::TaxonomiesHelper
+
+  def index
+    @master_variant = Variant.find(params[:product_id])
+    @taxonomies = @master_variant.product.taxonomies
+  end
+
+  def new
+    @master_variant = Variant.find(params[:product_id])
+    @taxonomy = Taxonomy.new
+    @taxonomies = sorted_taxonomies
+  end
+
+  def update
+    @master_variant = Variant.find(params[:product_id])
+    @taxonomy = Taxonomy.find(params[:variant][:taxonomy][:taxonomy])
+    @master_variant.product.taxonomies << @taxonomy
+    @master_variant.product.taxonomies << @taxonomy.ancestors
+    
+    respond_to do |format|
+      if @master_variant.product.save
+        format.html { redirect_to admin_product_taxonomies_url(@master_variant), notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @taxonomy.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @master_variant = Variant.find(params[:product_id])
+    @taxonomy = @master_variant.product.taxonomies.find(params[:id])
+    @master_variant.product.taxonomies.find(@taxonomy.children).delete rescue ''
+    @master_variant.product.taxonomies.find(@taxonomy.id).delete rescue ''
+
+    respond_to do |format|
+      format.html { redirect_to admin_product_taxonomies_url(@master_variant), notice: 'Taxonomy was successfull removed.'}
+      format.json { head :no_content}
+    end
+  end
+
+end
