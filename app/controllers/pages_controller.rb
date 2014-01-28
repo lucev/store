9308411ae5a06a7e73171ee300dab1390d2550db
Ivan Lučev <lucev.ivan@gmail.com>
2014-01-28 22:20:44 +0100

    Select taxonomies in search criteria

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
+  include Admin::TaxonomiesHelper
 
   def home
-    @variants = Variant.all
+    # @variants = Variant.all
+    if params[:taxonomies].present?
+      ids = []
+      params[:taxonomies].each do |taxonomy_id|
+        ids << Moped::BSON::ObjectId(taxonomy_id)
+      end
+      @variants = Variant.all('product.taxonomy_ids' => ids)
+    else
+      @variants = Variant.all
+    end
+    # @root_taxonomies = Taxonomy.where(parent_id: nil).order_by('created_at ASC')
+    @taxonomies = sorted_taxonomies
   end
 end
