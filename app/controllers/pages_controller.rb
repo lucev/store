3041b85394c591e3fa5e55ac6ff5e8ff347b7324
Ivan Lučev <lucev.ivan@gmail.com>
2014-02-02 03:01:15 +0100

    Fix search criteria

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,45 @@ class PagesController < ApplicationController
   include Admin::TaxonomiesHelper
 
   def home
-    # @variants = Variant.all
+    @variants = Variant.where(:is_master => :true)
     if params[:taxonomies].present?
-      ids = []
-      params[:taxonomies].each do |taxonomy_id|
-        ids << Moped::BSON::ObjectId(taxonomy_id)
+      taxonomy_ids = []
+      params[:taxonomies].each do |parent_taxonomy|
+        parent_taxonomy.values.each_with_index do |t, index|
+          taxonomy_ids[index] = []
+          t.each do |taxonomy_id|
+            taxonomy_ids[index] << Moped::BSON::ObjectId(taxonomy_id)
+          end
+        end
       end
-      @variants = Variant.all('product.taxonomy_ids' => ids)
-    else
-      @variants = Variant.all
+
+      taxonomy_selector = nil
+      taxonomy_ids.each_with_index do |ids, index|
+        taxonomy_selector = Variant.in({'product.taxonomy_ids' => taxonomy_ids[index]}).selector,
+                              taxonomy_selector
+      end
+      @variants = @variants.and(taxonomy_selector)
     end
-    # @root_taxonomies = Taxonomy.where(parent_id: nil).order_by('created_at ASC')
+
+    if params[:option_values].present?
+      option_value_ids = []
+      params[:option_values].each do |option_type|
+        option_type.values.each_with_index do |o, index|
+          option_value_ids[index] = []
+          o.each do |option_value_id|
+            option_value_ids[index] << Moped::BSON::ObjectId(option_value_id)
+          end
+        end
+      end
+
+      option_value_selector = nil
+      option_value_ids.each_with_index do |ids, index|
+        option_value_selector = Variant.in({'product.option_value_ids' => option_value_ids[index]}).selector,
+                                  option_value_selector
+      end
+      @variants = @variants.and(option_value_selector)
+    end
     @taxonomies = sorted_taxonomies
+    @option_types = OptionType.all
   end
 end
