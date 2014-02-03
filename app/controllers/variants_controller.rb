4f6994812a2b2aa92a96c72f5d2fe24168fe3e54
Ivan Lučev <lucev.ivan@gmail.com>
2014-02-03 18:30:55 +0100

    Fix vertical navbar at variant show page

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -1,8 +1,12 @@
 class VariantsController < ApplicationController
+  include Admin::TaxonomiesHelper
 
   def show
     @variant = Variant.find(params[:id])
     @variants = Variant.where(master_id: @variant.master_id)
+
+    @taxonomies = sorted_taxonomies
+    @option_types = OptionType.all
   end
   
 end
