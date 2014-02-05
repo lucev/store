48defa9caa1ef74be27237e77070361172f2fc5c
Ivan Lučev <lucev.ivan@gmail.com>
2014-02-05 15:55:31 +0100

    Add indexes on variants

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -15,10 +15,15 @@ class Variant
 
   embeds_one :product
   embeds_many :images
-  has_and_belongs_to_many :option_values  
+  has_and_belongs_to_many :option_values, index: true
 
   accepts_nested_attributes_for :product
   accepts_nested_attributes_for :option_values
+
+  index 'product.master_id' => 1
+  index 'product.name' => 1
+  index 'product.taxonomies' => 1
+  index 'product.option_types' => 1
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
