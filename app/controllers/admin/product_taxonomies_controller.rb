87375a6fdd1e902587f6199bad3fbe74dc36299d
Ivan Lučev <lucev.ivan@gmail.com>
2014-02-06 06:49:09 +0100

    Better updating product taxonomies

--- app/controllers/admin/product_taxonomies_controller.rb
+++ app/controllers/admin/product_taxonomies_controller.rb
@@ -15,15 +15,17 @@ class Admin::ProductTaxonomiesController < AdminController
   def update
     @master_variant = Variant.find(params[:product_id])
     @taxonomy = Taxonomy.find(params[:variant][:taxonomy][:taxonomy])
-    @variants = Variant.where(master_id: @master_variant.id)
-    @variants.each do |variant|
-      variant.product.taxonomies << @taxonomy
-      variant.product.taxonomies << @taxonomy.ancestors
-      variant.save
-    end
     
+    taxonomy_ids = [@taxonomy.id]
+    @taxonomy.ancestors.each do |taxonomy|
+      taxonomy_ids << taxonomy.id
+    end
+
+    Variant.where(master_id: @master_variant.id).push_all('product.taxonomy_ids', taxonomy_ids)
+    last_error = Variant.mongo_session.command(getLastError:1)
+
     respond_to do |format|
-      if true
+      if last_error['err'].nil?
         format.html { redirect_to admin_product_taxonomies_url(@master_variant), notice: 'Product was successfully updated.' }
         format.json { head :no_content }
       else
